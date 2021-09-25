6106663ae022236b1be4e6a40d3fdd72155c86b0
chaitali-kadam <kadamchpr19ce@student.mes.ac.in>
2021-09-25 20:22:49 +0530

    my first commit reactwebsite

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import classes from './App.css';
+import logo from './Capture.PNG';
+import React, { useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+const App = () => {
+  const [isDataLoad, setisDataLoad] = useState(false);
+  const [users, setUsers] = useState([]);
+  const [isBtnClicked, setisBtnClicked] = useState(false);
+
+  const loadUsers = async () => {
+    setisBtnClicked(true);
+    const response = await fetch('https://reqres.in/api/users?');
+    const res = await response.json();
+    // console.log(res)
+    setUsers(res.data);
+    setInterval(() => {
+      setisDataLoad(true);
+    }, 2000);
+  }
+  return ( 
+    <>
+     <div className="App">
+    <header className="App-header">
+        <nav className = {classes.Topbar}>
+        <img src = {logo} alt ="logo" width="140px" height="60px" style={{marginLeft:"40px"}}></img> 
+         <button className="Get-user" onClick={loadUsers}><b> Get Users </b></button> 
+      </nav>
       </header>
+    {
+      isBtnClicked ? (
+        isDataLoad ? ( 
+          <div className="row">
+          {users?.map((current, index) => {
+            return (
+              <div className="card column" key={index}>
+                <img src={current.avatar} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h3 className="card-title">
+                    {current.first_name} {current.last_name}
+                  </h3>
+                  <h4 className="card-text">{current.email}</h4>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+     
+        ) : (
+          <div className ="loader">
+          </div>
+        )
+      ) : ( 
+        <section > 
+        </section>
+      )
+    } 
     </div>
+    </>
+    
   );
 }
-
 export default App;
